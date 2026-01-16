5a73303929cc7b2410ba170b0b657a801ddabaf2
Riyadul Islam Riyadh <riyadulislamriyadh@Riyaduls-MacBook-Pro.local>
2026-01-16 07:08:54 +0600

    Add JWT authentication and enforce SECRET_KEY validation

--- onesuite_backend/settings.py
+++ onesuite_backend/settings.py
@@ -9,7 +9,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config('SECRET_KEY', default='django-insecure-default-key')
+# SECRET_KEY is REQUIRED - will raise error if not set in environment
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)
